1583ce62dab5b4c128aa84bfd9142539e3bbf65b
James Smith <james@floppy.org.uk>
2017-09-14 11:26:58 +0100

    test query combos with search

--- test/unit/task_test.rb
+++ test/unit/task_test.rb
@@ -8,8 +8,17 @@ class TaskTest < ActiveSupport::TestCase
     Task.create(name: "Batmanuel") # from The Tick, look it up
     # Case insensitive substring search
     # Tests could be brittle because of ordering
-    assert Task.search("bat").map{|x| x.name} == ["Batman", "Batmanuel"]
-    assert Task.search("robin").map{|x| x.name} == ["Robin"]
+    assert Task.search("bat").map(&:name) == ["Batman", "Batmanuel"]
+    assert Task.search("robin").map(&:name) == ["Robin"]
+  end
+  
+  test "search with other query params" do
+    Task.create(name: "Batman", complete: false)
+    Task.create(name: "Robin", complete: true)
+    assert Task.where(complete: true).search("bat").map(&:name) == []
+    assert Task.where(complete: false).search("bat").map(&:name) == ["Batman"]
+    assert Task.where(complete: true).search("rob").map(&:name) == ["Robin"]
+    assert Task.where(complete: false).search("rob").map(&:name) == []
   end
 
 end
