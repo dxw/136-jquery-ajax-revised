8a076f0d4b7836effd49baf528156bf0998a7e6f
James Smith <james@floppy.org.uk>
2017-09-14 11:20:01 +0100

    Add a search method to Task

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,9 @@
 class Task < ActiveRecord::Base
+
+  def self.search(query)
+    # This is really simplistic, we just look for substring in the name
+    tasks = Task.arel_table
+    Task.where(tasks[:name].matches("%#{query}%"))
+  end
+
 end
--- test/unit/task_test.rb
+++ test/unit/task_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class TaskTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "search" do
+    Task.create(name: "Batman")
+    Task.create(name: "Robin")
+    Task.create(name: "Batmanuel") # from The Tick, look it up
+    # Case insensitive substring search
+    # Tests could be brittle because of ordering
+    assert Task.search("bat").map{|x| x.name} == ["Batman", "Batmanuel"]
+    assert Task.search("robin").map{|x| x.name} == ["Robin"]
+  end
+
 end
